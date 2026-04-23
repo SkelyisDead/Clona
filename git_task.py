b3b0f2b4fcc8b17782dccfd53db2c10fc0cd448e
Radu Ungureanu <radu.ungureanu@socopon.com>
2026-04-23 08:41:08 +0300

    Make it run every 5 sec
    Signed-off-by: Radu Ungureanu <radu.ungureanu@socopon.com>

--- git_task.py
+++ git_task.py
@@ -15,8 +15,20 @@ surface = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Procedural Color Grid (Press SPACE to Regenerate)")
 data = make_empty_grid()
 running = True
+last_tick = pygame.time.get_ticks()
 
 while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            data = make_empty_grid()
+
+    now = pygame.time.get_ticks()
+    if now - last_tick >= 5000:
+        data = make_empty_grid()
+        last_tick = now
+
     surface.fill((0, 0, 0))
 
     for y in range(10):
@@ -24,11 +36,5 @@ while running:
              pygame.draw.rect(surface, data[y][x], (x*50, y*50, 50, 50))
 
     pygame.display.flip()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            data = make_empty_grid()
 
 pygame.quit()
