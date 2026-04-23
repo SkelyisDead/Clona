2706387e81019a23b96513807d18e2b28dd9d81f
PeterParty <petrisor.caraiman@stud.fiir.upb.ro>
2026-04-23 13:48:28 +0300

    Schimbare variabile si comentari

--- git_task.py
+++ git_task.py
@@ -1,13 +1,21 @@
 import pygame,random
+
 pygame.init()
-def f():
+def creat_matix_of_color():
  return [[(random.randint(0,255),random.randint(0,255),random.randint(0,255)) for _ in range(10)] for _ in range(10)]
-s=pygame.display.set_mode((500,500));pygame.display.set_caption("Procedural Color Grid (Press SPACE to Regenerate)");data=f();r=True
-while r:
- s.fill((0,0,0))
+
+#Creaza fereastrra si creaza titlul
+window=pygame.display.set_mode((500,500));pygame.display.set_caption("Procedural Color Grid (Press SPACE to Regenerate)");
+data=creat_matix_of_color();
+start=True
+
+while start:
+ window.fill((0,0,0))
  for y in range(10):
-  for x in range(10):pygame.draw.rect(s,data[y][x],(x*50,y*50,50,50))
+  for x in range(10):pygame.draw.rect(window,data[y][x],(x*50,y*50,50,50))
  pygame.display.flip()
- for e in pygame.event.get():
-  r=False if e.type==pygame.QUIT else r;data=f() if e.type==pygame.KEYDOWN and e.key==pygame.K_SPACE else data
+ 
+ for event in pygame.event.get():
+  start=False if event.type==pygame.QUIT else start;
+  data=creat_matix_of_color() if event.type==pygame.KEYDOWN and event.key==pygame.K_SPACE else data
 pygame.quit()
