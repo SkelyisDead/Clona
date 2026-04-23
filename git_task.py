b74dd3e1c0aab14ebce597f866bd61fa9bd19c43
Burada Rares Andrei <raresburada61@gmail.com>
2026-04-23 13:28:12 +0300

    Add files via upload

--- git_task.py
+++ git_task.py
@@ -1,13 +1,32 @@
-import pygame,random
+import pygame, random
 pygame.init()
-def f():
- return [[(random.randint(0,255),random.randint(0,255),random.randint(0,255)) for _ in range(10)] for _ in range(10)]
-s=pygame.display.set_mode((500,500));pygame.display.set_caption("Procedural Color Grid (Press SPACE to Regenerate)");data=f();r=True
-while r:
- s.fill((0,0,0))
+def generare_culoare():
+ return [[(random.randint(0,255), random.randint(0,255), random.randint(0,255)) for _ in range(10)] for _ in range(10)]
+#genereaza o matrice de 10 linii si 10 coloane, care tine minte in fiecare element valorile unei culori RGB
+
+s=pygame.display.set_mode((500,500));
+pygame.display.set_caption("Procedural Color Grid (Press SPACE to Regenerate)")
+data=generare_culoare();
+rulare=True
+
+EVENIMENT_REGEN = pygame.USEREVENT + 1
+
+pygame.time.set_timer(EVENIMENT_REGEN, 5000);
+
+while rulare:
+ s.fill((0,0,0)) #un spatiu de a pune culoarea
  for y in range(10):
-  for x in range(10):pygame.draw.rect(s,data[y][x],(x*50,y*50,50,50))
+  for x in range(10): 
+    pygame.draw.rect(s,data[y][x],(x*50,y*50,50,50)) #se deseneaza pe templateul s culoarea memorata in data
+
  pygame.display.flip()
+
  for e in pygame.event.get():
-  r=False if e.type==pygame.QUIT else r;data=f() if e.type==pygame.KEYDOWN and e.key==pygame.K_SPACE else data
+
+  if e.type == pygame.QUIT: #verificam daca s-a terminat jocul
+    rulare = False
+
+  if e.type == pygame.KEYDOWN and e.key == pygame.K_SPACE: # verificam daca se apasa space
+    data = generare_culoare()
+
 pygame.quit()
